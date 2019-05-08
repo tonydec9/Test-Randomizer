47b6274bfe69d8122c88c637b2fd4ffef52bf3c4
Eduardo Garcia <eduardo.garcia16@stjohns.edu>
2019-05-08 16:01:23 -0400

    added verifylogin method to return user type

--- src/cus1156_groupproject/ExamModel.java
+++ src/cus1156_groupproject/ExamModel.java
@@ -2,30 +2,52 @@ package cus1156_groupproject;
 
 import java.io.*;
 
-class ExamModel {
+public class ExamModel {
 	
-	FileWriter fw;
+	static FileWriter fw;
 	
-	public void createExam(String name){
-		try{
+	public String verifyLogin(String user, String pass){
+		if(user.equals("professor") && pass.equals("teachersonly")){
+			return "professor";
+		}
+		else if(user.equals("student") && pass.equals("12345")){
+			return "student";
+		}
+		else{
+			return "Invalid Login - Try Again";
+		}
+	}
+	
+	public static void createExam(String name){
+		try
+		{
 			fw = new FileWriter("Exam.txt");
+			fw.write(name);
 		}
 		catch (IOException e){
 			System.out.println("Error");
 		}
 	}
 	
-	public void addQuestion(String question, String answer1, String answer2, String answer3){
-		try{
+	public static void addQuestion(String question, String answer1, String answer2, String answer3){
+		try
+		{
 			fw.write(question);
 			fw.write(answer1);
 			fw.write(answer2);
 			fw.write(answer3);
+			fw.close();
 		}
 		catch(IOException e){
 			System.out.println("error");
 		}
 		
+	}
+	
+	public static void main(String[] args)
+	{
+		createExam("Exam 1");
+		addQuestion("Yes?", "No", "Maybe", "Yes");
 	}
 
 }
