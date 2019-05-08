8224209456c8ba08962cc7c9a45670dff78789c2
aalva577 <aalva577@aalva577-macbookpro-mmxv.fios-router.home>
2019-05-08 15:14:01 -0400

    Updated ExamController and ExamView. Waiting on Model.

--- src/cus1156_groupproject/ExamController.java
+++ src/cus1156_groupproject/ExamController.java
@@ -1,5 +1,41 @@
 package cus1156_groupproject;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JTextField;
 
 public class ExamController {
 
+	private ExamView theView;
+	private ExamModel themodel;
+	
+	public ExamController (ExamView theView, ExamModel themodel) {
+		
+		this.theView = theView;
+		this.themodel = themodel;
+		
+		
+		this.theView.addExamListener(new ExamListener ());
+
+	}
+	
+	class ExamListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		
+			String question1, answer1, answer2, answer3;
+			
+			try {
+				question1 = theView.getQuestion1();
+				answer1 = theView.getAnswer1();
+				answer2 = theView.getAnswer2();
+				answer3 = theView.getAnswer3();
+			}
+			 catch(NumberFormatException ex){
+
+				 System.out.println(ex);
+				 theView.displayErrorMessage("You Need to fill-in a question and three answers");				
+}	
+}		
+}	
 }
