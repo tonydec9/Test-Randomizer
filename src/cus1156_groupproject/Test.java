ce0c7de5353d05777473c01ecad5183fa60d1d2e
jacos105 <jacos105@jacos105-macbookpro-f-mmxvii.mobile.stjohns.edu>
2019-05-08 22:10:14 -0400

    test

--- src/cus1156_groupproject/Test.java
+++ src/cus1156_groupproject/Test.java
@@ -1,5 +1,14 @@
 package cus1156_groupproject;
 
-public class Test {
+public class Test extends ExamController {
+	
+	public static void main(String[] args) {
 
-}
+		ExamView theView = new ExamView();
+		ExamModel theModel = new ExamModel();
+		
+		ExamController theController = new ExamController();
+		
+		theView.setVisible(true);
+		
+	}
